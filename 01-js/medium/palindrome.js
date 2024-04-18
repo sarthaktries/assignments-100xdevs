accdcd75fb923e1a45d3ba9fcddfdf6dfe0cc53f
Sarthak Gupta <sarthak.writter1@gmail.com>
2024-04-18 14:35:59 +0530

    Solved 01-js/medium/palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,39 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
+function evenPalindrome (str, strLength) {
+  let loMid = strLength / 2 - 1;
+  let hiMid = strLength / 2;
+  for (let x = loMid; x >= 0; x--) {
+    if (str[loMid] != str[hiMid]) {
+      return false;
+    }
+    loMid -= 1;
+    hiMid += 1;
+  }
   return true;
+}
+
+function oddPalindrome (str, strLength) {
+  let mid = Math.floor(strLength / 2);
+  for (let x = mid; x >= 0; x--) {
+    if (str[x] != str[mid]) {
+      return false;
+    }
+    mid += 1;
+  }
+  return true;
+}
+
+function isPalindrome(str) {
+  str = str.replace(/\s/g, '').toLowerCase();
+  str = str.replace(/[\.,?!]/g, '');
+  const strLength = str.length;
+  if (strLength % 2 == 0) {
+    return evenPalindrome(str, strLength);
+  } else {
+    return oddPalindrome(str, strLength);
+  }
 }
 
 module.exports = isPalindrome;
