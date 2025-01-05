fcd91bfbd275ba894c66480fb27c1378802d8bc6
Sarthak <sarthak.writter1@gmail.com>
2025-01-05 19:02:54 +0530

    Solved 01-js/easy/anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,34 @@
 
 function isAnagram(str1, str2) {
 
+  str1 = str1.replace("/\s/g", "").toLowerCase();
+  str2 = str2.replace("/\s/g", "").toLowerCase();
+
+  if (str1.length != str2.length) {
+    return false;
+  }
+
+  const map1 = {};
+
+  for (let char of str1) {
+    if (map1.hasOwnProperty(char)) {
+      map1[char] = map1[char] + 1;
+    } else {
+      map1[char] = 1;
+    }
+  }
+
+  for (let char of str2) {
+    if (map1.hasOwnProperty(char)) {
+      map1[char] = map1[char] - 1;
+      if (map1[char] < 0) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
