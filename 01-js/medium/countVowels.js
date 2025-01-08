893297938f8b7906b85d84911d870b9e53009968
Sarthak <sarthak.writter1@gmail.com>
2025-01-08 17:31:58 +0530

    Solved 01-js/medium/countVowels.js

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,16 @@
 */
 
 function countVowels(str) {
-    // Your code here
+
+  str = str.toLowerCase();
+  let count = 0;
+
+  for (let char of str) {
+    if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+      count = count + 1;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
